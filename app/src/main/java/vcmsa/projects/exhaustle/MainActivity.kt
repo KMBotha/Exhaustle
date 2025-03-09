fb2621200b74c2049f2de5ab60bf4070eefbb989
KMBotha <st10258494@vcconnect.edu.za>
2025-03-09 10:24:19 +0200

    Initial commit

---
+++ app/src/main/java/vcmsa/projects/exhaustle/MainActivity.kt
@@ -0,0 +1,33 @@
+package vcmsa.projects.exhaustle
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+private val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate called")
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var btnBegin : Button = findViewById(R.id.btnBegin)
+        btnBegin.setOnClickListener() {
+            Log.d(TAG, "Game has started. Generated word is: ")
+            val intent = Intent(this, GameWindow :: class.java)
+            startActivity(intent)
+        }
+    }
+}
