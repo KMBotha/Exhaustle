ef568db8f6b37c1281aa6ecdb31af7ece44a5865
Kyugo129 <126760274+Kyugo129@users.noreply.github.com>
2025-03-10 00:42:57 +0200

    202503100042 gamescreen changes as well as begin of validation implementation

--- app/src/main/java/vcmsa/projects/exhaustle/GameWindow.kt
+++ app/src/main/java/vcmsa/projects/exhaustle/GameWindow.kt
@@ -1,12 +1,20 @@
 package vcmsa.projects.exhaustle
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.gson.Gson
+import java.net.URL
+import java.util.concurrent.Executors
 
 class GameWindow : AppCompatActivity() {
+    val guessNum : Number = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,17 @@ class GameWindow : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun wordleStart(){
+        val guessedWord = findViewById<TextView>(R.id.txtGuess).text
+        val executor = Executors.newSingleThreadExecutor()
+        executor.execute {
+            val url = URL("https:") //insert the actual url for the api later
+            val json = url.readText()
+            Log.d("GameWIndow", json.toString())
+            val ValidationArr = Gson().fromJson(json, Array<String>::class.java)
+        }
+
     }
 }
